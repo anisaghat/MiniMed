f370a1ab05ea290e756270ab1a9caa3b45aa5afd
anisa.ghat <anisa.ghatchoekajeva@student.hepl.be>
2026-08-18 00:52:23 +0200

    visibilité du mot de passe

--- lib/widget/my_form_field.dart
+++ lib/widget/my_form_field.dart
@@ -25,15 +25,38 @@ class MyFormField extends StatefulWidget {
 }
 
 class _MyFormFieldState extends State<MyFormField> {
+  late bool isHidden;
+
+  @override
+  void initState() {
+    super.initState();
+    isHidden = widget.isToHide;
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       initialValue: widget.initialValue,
       keyboardType: widget.keyboardType,
-      obscureText: widget.isToHide,
+      obscureText: isHidden,
       onChanged: widget.onChanged,
       decoration: InputDecoration(
         prefixIcon: Icon(widget.icon, color: iconFormColor),
+        suffixIcon: widget.isToHide
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    isHidden = !isHidden;
+                  });
+                },
+                icon: Icon(
+                  isHidden
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined,
+                  color: iconFormColor,
+                ),
+              )
+            : null,
         hintText: widget.label,
         hintStyle: TextStyle(fontSize: fontSizeS, color: textFormColor),
       ),
